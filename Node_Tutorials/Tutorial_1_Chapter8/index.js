170d5182978a10f0c4f1ab285306fc79801ca11a
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-04-27 09:12:32 +0200

    node

--- Node_Tutorials/Tutorial_1_Chapter8/index.js
+++ Node_Tutorials/Tutorial_1_Chapter8/index.js
@@ -2,35 +2,35 @@ const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const fileUpload = require("express-fileupload")
 const BlogPost = require('./models/BlogPost.js');
-
-
-
-//const BlogPost = mongoose.model('BlogPost',BlogPostSchema); 
+ 
 const Schema = mongoose.Schema;
 const app = new express();
+const fileUpload = require("express-fileupload");
 
-const BlogPostSchema = new Schema({ 
-    title: String, 
-    body: String, 
-    username: String, 
-    datePosted:{ /* can declare property type with an object like this 
-    because we need 'default' */ 
-    type: Date, 
-    default: new Date() 
-    }   
-    }); 
+const customMiddleWare = (req, res, next) => {
+    console.log("Custom middle ware called")
+    next()
+}
+
+const validationMiddleWare = (req, res, next) => {
+    if(req.files == null || req.body.title == null) {
+        return res.redirect("/posts/new")
+    }
+    next()
+}
 
 app.use(express.static('public'));
 app.use(express.json()); 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded());
 app.use(fileUpload());
+app.use(customMiddleWare)
+app.use("/posts/store", validationMiddleWare)
 // With app.set('view engine','ejs'), we tell Express to use EJS as our templating engine, 
 // that any file ending in .ejs should be rendered with the EJS package.
 app.set('view engine', 'ejs');
 mongoose.connect('mongodb://127.0.0.1/my_database', {useNewUrlParser: true});
-module.exports = BlogPost 
+
 //app.get('/', (req, res) => {
 //    res.render('index');
 //});
@@ -40,7 +40,7 @@ app.get('/',async (req,res)=>{
     res.render('index',{ 
     blogposts: blogposts 
     }); 
-    }) 
+    })
 
 app.get('/about', (req, res) => {
     res.render('about');
@@ -54,12 +54,12 @@ app.get('/post', (req, res) => {
     res.render('post');
 });
 
-app.get('/post/:id',async (req,res)=>{         
+/*app.get('/post/:id',async (req,res)=>{         
     const blogpost = await BlogPost.findById(req.params.id) 
     res.render('post',{ 
     blogpost 
-    })     
-    }) 
+})     
+    })*/
 
 app.get("/posts/new", (req, res) => {
     res.render("create");
@@ -70,16 +70,16 @@ app.get("/posts/new", (req, res) => {
     //res.redirect('/') 
     //})
 
-app.post('/posts/store', async (req,res)=>{             
+app.post("/posts/store", (req,res) => {
     let image = req.files.image;
-    image.mv(path.resolve(__dirname, "public/img", image.name),
-      async (error) => {
-        await BlogPost.create({
-            ...req.body,
-            image: "/img/" + image.name
-        })
-        res.redirect("/")
-      }) 
+    image.mv(path.resolve(__dirname, "public/img", image.name), 
+    async(error) => {
+    await BlogPost.create({
+        ...req.body,
+        image: "/img/" + image.name
+    })
+    res.redirect("/")
+})
 })
 
 app.listen(3000, () => {
