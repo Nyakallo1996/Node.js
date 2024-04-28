9c34d245210c2963144bf6bac6274af4b22c6ca8
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-04-28 22:35:46 +0200

    node

--- Node_Tutorials/Tutorial_1_Chapter10/index.js
+++ Node_Tutorials/Tutorial_1_Chapter10/index.js
@@ -1,80 +1,68 @@
-const express = require('express');
+const express = require("express");
 
-const app = new express();
-const ejs = require('ejs');
-const mongoose = require('mongoose');
-const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 const fileUpload = require("express-fileupload");
+const validationMiddleWare = require("./middleware/validationMiddleware");
 const newPostController = require("./controllers/newPost");
 const homeController = require("./controllers/home");
 const storePostController = require("./controllers/storePost");
 const getPostController = require("./controllers/getPost");
-const validateMiddleWare = require("./middleware/validationMiddleware");
-const BlogPost = require('./models/BlogPost.js');
-const newUserController = require("./controllers/newUser")
-const storeUserController = require("./controllers/storeUser");
-const loginController = require("./controllers/login");
-const loginUserController = require("./controllers/loginUser");
- 
+//const BlogPost = mongoose.model('BlogPost',BlogPostSchema);
 const Schema = mongoose.Schema;
-app.use(fileUpload());
+const app = new express();
 
-mongoose.connect('mongodb://127.0.0.1/my_database', {useNewUrlParser: true});
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.set('view engine', 'ejs');
-
-app.use(express.static('public'));
-
-app.listen(3000, () => {
-    console.log("App listening on port 3000");
-});
-
-const customMiddleWare = (req, res, next) => {
-    console.log("Custom middle ware called")
-    next()
+const customMiddleware = (req, res, next) => {
+  console.log("Custom middle ware called")
+  next()
 }
 
-app.get("/auth/register", newUserController);
-
-app.post("/users/register", storeUserController);
-
-app.get("/auth/login", loginController);
-
-app.post("/users/login", loginUserController)
 
 
 
 
+app.use(customMiddleware);
+
+app.use(fileUpload());
 
 
 
-
-
-app.use(express.json()); 
-app.use(express.urlencoded());
-
-app.use(customMiddleWare)
-app.use("/posts/store", validateMiddleWare)
-// With app.set('view engine','ejs'), we tell Express to use EJS as our templating engine, 
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// With app.set('view engine','ejs'), we tell Express to use EJS as our templating engine,
 // that any file ending in .ejs should be rendered with the EJS package.
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+mongoose.connect("mongodb://127.0.0.1/my_database", { useNewUrlParser: true });
+//app.get('/', (req, res) => {
+//    res.render('index');
+//});
 
 
+/*app.get("/", async (req, res) => {
+  const blogposts = await BlogPost.find({});
+  res.render("index", {
+    blogposts: blogposts,
+  });
+});*/
+
+app.use("/posts/store", validationMiddleWare);
+
+
+
+
+app.get("/posts/new", newPostController); 
 
 app.get("/", homeController);
 
-
 app.get("/post/:id", getPostController);
-
-
-
-
-app.get("/posts/new", newPostController);
-
-
 
 app.post("/posts/store", storePostController);
 
 
+
+
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000");
+});
