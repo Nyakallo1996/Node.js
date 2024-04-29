43611052b0f811203f1ef7813128a0235616fa63
Nyakallo1996 <Lekgoabamahlaks@gmail.com>
2024-04-29 21:29:17 +0200

    node

--- Node_Tutorials/Tutorial_1_Chapter13/middleware/authMiddleware.js
+++ Node_Tutorials/Tutorial_1_Chapter13/middleware/authMiddleware.js
@@ -1,13 +1,4 @@
-const User = require("../models/User")
-
-/*module.exports = (req, res, next) => {
-    User.findById(req.session.userId, (error, user) => {
-        if(error || !user)
-        return res.redirect("/")
-
-        next()
-    })
-}*/
+const User = require("../models/User");
 
 module.exports = async (req, res, next) => {
     try {
@@ -17,7 +8,7 @@ module.exports = async (req, res, next) => {
         }
         next();
     } catch (error) {
-        // Handle the error (e.g., log it or send an error response)
+        
         console.error('Error finding user:', error);
         res.status(500).send('Internal server error');
     }
--- Node_Tutorials/Tutorial_1_Chapter13/models/User.js
+++ Node_Tutorials/Tutorial_1_Chapter13/models/User.js
@@ -1,30 +1,21 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-var uniqueValidator = require("mongoose-unique-validator");
 const bcrypt = require("bcrypt");
 
 const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: [true, "Please provide username"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Please provide password"],
-  },
+    username: String,
+    password: String,
+ 
 });
 
-UserSchema.plugin(uniqueValidator);
+UserSchema.pre("save", function(next) {
+    const user = this
 
-UserSchema.pre("save", function (next) {
-  const user = this;
-  bcrypt.hash(user.password, 10, (error, hash) => {
-    user.password = hash;
-    next();
-  });
-});
+    bcrypt.hash(user.password, 10, (error, hash) => {
+        user.password = hash
+        next()
+    })
+})
 
-// export model
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model('User', UserSchema);
 module.exports = User;
